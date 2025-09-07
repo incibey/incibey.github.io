eacb5ef4eeefd73054ede20be9fea73a13ac031e
incibey <zincimend@gmail.com>
2025-09-08 02:32:22 +0300

    Update push-worker.js

--- push-worker.js
+++ push-worker.js
@@ -4,3 +4,18 @@ self.importScripts(`${self[`hostUrl`]}/worker.js`);
 // uncomment and set path to your service worker
 // if you have one with precaching functionality (has oninstall, onactivate event listeners)
 // self.importScripts('path-to-your-sw-with-precaching')
+
+// Dakikalık bildirim tetikleyici (yalnızca aktifken çalışır)
+self.addEventListener('activate', () => {
+  startMinuteLoop();
+});
+
+function startMinuteLoop() {
+  setInterval(() => {
+    self.registration.showNotification('Dakikalık Bildirim', {
+      body: 'Her dakika bir niyet, bir hatırlatma.',
+      icon: '/icon.png',
+      badge: '/badge.png'
+    });
+  }, 60000); // 60 saniye
+}
